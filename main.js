974aeec5cb37a16ca3c374ba1d3b17ff2360c1c2
Kobayashi Haruaki <rigelpain@gmail.com>
2021-08-15 22:17:36 +0900

    [update] nakata root day38

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function draw() {
   var DIR = "../img/indonesia/";
   nodes = [
     { id: "kobayashi", shape: "circularImage", image: "missing.png", label: "小林 陽昭", 
-      title: "2年\n",
+      title: "2年\nラーメン食べがち",
     },
     { id: "ayatsuji", shape: "circularImage", image: "./img/ayatsuji.jpg", label: "絢辻 詞", 
       title: 
@@ -21,7 +21,7 @@ function draw() {
       },
     },
     { id: "sakurai", shape: "circularImage", image: "./img/sakurai.jpg", label: "桜井 梨穂子", 
-      title: "2年\n水泳が苦手\n母が冷え性",
+      title: "2年\n水泳が苦手\n母が冷え性\n可にクリームコロッケが好き\nお小遣い1日200円",
       color: {
         border: "#EEAA99",
       },
@@ -33,7 +33,7 @@ function draw() {
       },
     },
     { id: "nakata", shape: "circularImage", image: "./img/nakata.jpg", label: "中多 紗江", 
-      title: "1年\nサラダサンドと牛乳\nコンビニのおでんが好き\n交際経験なし\n",
+      title: "1年\n交際経験なし\nエスカレーターお嬢様学校から編入\n女子高時代女子からの過剰なスキンシップ\n父が過干渉\nパパ呼び\nごはん時に野球中継をよく見る\nお小遣いは月2万\n体が柔らかい\n好き：\n　サラダ・玉子サンド，牛乳，コンビニおでん，ケーキ，パフェ\n苦手：\n　運動，男，年上\n初恋：\n　幼稚園，戦隊ヒーロー俳優",
       color: {
         border: "#EE9999",
       },
@@ -64,6 +64,15 @@ function draw() {
     { id: "tsukahara", shape: "circularImage", image: "./img/tsukahara.jpg", label: "塚原 響", 
       title: "3年"
     },
+    { id: "yuduki", shape: "circularImage", image: "./img/yuduki.jpg", label: "夕月 琉璃子", 
+      title: "3年"
+    },
+    { id: "hiba", shape: "circularImage", image: "./img/hiba.jpg", label: "飛羽 愛歌", 
+      title: "3年"
+    },
+    { id: "yukari", shape: "circularImage", image: "./img/yukari.jpg", label: "絢辻 縁", 
+      title: ""
+    },
     { id: "sosetsu-sai", shape: "circularImage", image: "missing.png", label: "創設祭",
       title: "12/24\n文化祭"
     },
@@ -85,16 +94,20 @@ function draw() {
     { from: 'morishima', to: "tsukahara", label: "親友" },
 
     { from: 'nakata', to: "miya", label: "同級生" },
-
+    
     { from: 'tanamachi', to: "tanaka", label: "親友" },
-
+    
     { from: 'ayatsuji', to: "umehara", label: "同級生" },
     { from: 'ayatsuji', to: "sosetsu-sai", label: "委員" },
-
+    { from: 'ayatsuji', to: "yukari", label: "姉妹" },
+    
     { from: 'sakurai', to: "itoh", label: "親友" },
-
+    { from: 'sakurai', to: "yuduki", label: "茶道部" },
+    { from: 'sakurai', to: "hiba", label: "茶道部" },
+    { from: 'yuduki', to: "hiba", label: "茶道部" },
+    
     { from: 'nanasaki', to: "tsukahara", label: "水泳部" },
-
+    
 
 
     { from: 2, to: 3 },
